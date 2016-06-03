85f5fca6c0855d4278343846def2b6dd2dd3a144
Kiki Gerritsen <k.gerritsen@lukkien.com>
2016-06-03 15:42:18 +0200

    finished template

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const electron = require('electron')
-
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
 let mainWindow
 
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
